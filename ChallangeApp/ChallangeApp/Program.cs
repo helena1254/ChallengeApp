16ffaff253b0d10cd41094c7fc2ea7d5402bb224
Marena Łukaszczuk-Kobiela <marlena_lukaszczuk@op.pl>
2023-10-19 21:23:56 +0200

    Task day 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "women";
+
+var age = 29;
+
+
+if (sex == "women" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33 && sex == "women")
+{
+    Console.WriteLine(name + ",lat " + age);
+}
+else if (sex == "men" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+else
+{
+    Console.WriteLine("brak danych");
+}
