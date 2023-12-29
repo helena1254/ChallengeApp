0bb98c02a9d0d6e86a1ec3a4f81ac0193a0fb5f8
Marena Łukaszczuk-Kobiela <marlena_lukaszczuk@op.pl>
2023-12-29 13:25:33 +0100

    Zadanie dzień 6

--- ChallangeApp/ChallangeApp/Employee.cs
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -1,10 +1,12 @@
-﻿using ChallangeApp;
+﻿namespace ChallangeApp
+{
+    public class Employee
+    {
+        Employee employee1 = new Employee("Adam", "Małysz", 30);
+        Employee employee2 = new Employee("Anna", "Adamska", 31);
+        Employee employee3 = new Employee("Damian", "Rompalski", 32);
 
-Employee employee1 = new Employee("Adam", "Małysz", 30);
-Employee employee2 = new Employee("Anna", "Adamska", 31);
-Employee employee3 = new Employee("Damian", "Rompalski", 32);
-
-employee1.AddScore(4);
+        employee1.AddScore(4);
 employee1.AddScore(1);
 employee1.AddScore(2);
 employee1.AddScore(3);
@@ -28,17 +30,16 @@ List<Employee> employees = new List<Employee>()
 {
     employee1, employee2, employee3
 };
-int maxResult = -1;
-Empoloyee employeeWithMaxResult = null;
-
+        int maxResult = -1;
+        Empoloyee emploeeWithMaxResult = null;
 foreach(var employee in employees)
 {
     if (employee.Result > maxResult)
     {
-        employeeWithMaxResult= employee;
+        emploeeWithMaxResult= employee;
         maxResult=employee.Result;
 
     }
 }
-
-Console.WriteLine
+    }
+}
